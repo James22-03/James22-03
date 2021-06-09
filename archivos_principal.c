3f7fde668bd5393882b2f4dc1133f1b9baf8afb1
James22-03 <73191209+James22-03@users.noreply.github.com>
2021-06-08 22:43:46 -0500

    Add files via upload

---
+++ archivos_principal.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char *nombre;
+char *nombre2;
+int existe_archivo(char *nombre_arch);
+int crear_archivo(char *nombre_arch);
+void liberar();
+void insertar();
+void imprimir();
+void impfile(FILE *ptrArchivo, char *nombre_arch);
+int vacia();
+void cerrar_archivo(FILE *ptrArchivo, char *nombre_arch);
+FILE *abrir_Archivo_solo_Lectura(char *nombre_arch);
+FILE *abrir_Archivo_lectura_escritura(char *nombre_arch);
+
+struct nodo {
+	char cadena[50];
+	struct nodo *sig;
+};
+
+struct nodo *raiz=NULL;
+
+int main (int argc, char *argv[]){
+int v[100],n,j,temp;
+FILE *ptrCf = NULL,*g = NULL;
+nombre=argv[1];
+nombre2=argv[2];
+int existe_arch = 0,i;
+existe_arch = existe_archivo(nombre);
+
+if(existe_arch){
+	printf("---------------------------------------------------------------\n");
+	printf("\tEl archivo -> %s <- YA EXISTE.\n", nombre);
+	printf("---------------------------------------------------------------\n");
+	}else{
+		printf("---------------------------------------------------------------\n");
+		printf("\tEl archivo -> %s <- NO EXISTE.\n", nombre);
+		printf("---------------------------------------------------------------\n");
+		if(crear_archivo(nombre)){
+			printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+			printf("\tEl archivo -> %s <- Fue creado EXITOSAMENTE.\n", nombre);	
+			printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+			}else{
+				printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+				printf("\tEl archivo -> %s <- NO pudo crearse.\n",nombre);			
+				printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+			}
+			}
+		ptrCf = abrir_Archivo_solo_Lectura(nombre);
+		if(ptrCf == NULL){
+			printf("****************************************************************\n");
+			printf("\tEl archivo -> %s <- NO pudo Abrirse.\n", nombre);			
+			printf("****************************************************************\n");
+		}
+		else{
+			printf("****************************************************************\n");
+			printf("\tArchivo -> %s <- Abierto Correctamente.\n", nombre);			
+			printf("****************************************************************\n");
+		ptrCf = abrir_Archivo_lectura_escritura(nombre);
+		if(ptrCf == NULL){
+		printf("****************************************************************\n");
+		printf("\tEl archivo -> %s <- NO pudo Abrirse.\n", nombre);			
+		printf("****************************************************************\n");
+		}
+		else{
+		printf("****************************************************************\n");
+		printf("\tArchivo -> %s <- Abierto Correctamente.\n", nombre);			
+		printf("****************************************************************\n");		
+		printf("----------------------------> ESCRITURA <----------------------------------\n");
+		fprintf(ptrCf,"Me entere que estaban buscando personal a traves del grupo embedidos32.");	
+		}	
+		printf("\tEscritura Terminada.\n");
+		cerrar_archivo(ptrCf, nombre);
+		}
+		ptrCf = abrir_Archivo_lectura_escritura(nombre2);
+		if(ptrCf == NULL){
+		printf("\tEl archivo -> %s <- NO pudo Abrirse.\n", nombre2);
+		}
+		else{
+		printf("\tArchivo -> %s <- Abierto Correctamente.\n", nombre2);		
+		printf("----------------------------> ESCRITURA <----------------------------------\n");
+		insertar("Me");
+		insertar("entere");
+		insertar("que");
+		insertar("estaban");
+		insertar("buscando");
+		insertar("personal");
+		insertar("a");
+		insertar("traves");
+		insertar("del");
+		insertar("grupo");
+		insertar("embedidos32.");
+		imprimir();
+		
+		struct nodo *reco=raiz;
+		while (reco != NULL){
+		fprintf(ptrCf,"%s\n",reco->cadena);
+		reco=reco->sig;
+		}
+		printf("\n");	
+		printf("\tEscritura Terminada.\n");
+		cerrar_archivo(ptrCf, nombre2);
+		}	
+		return 0;
+}
+
+
+int existe_archivo(char *nombre_arch){
+	FILE *ptrArchivo;
+	int existe = 0;
+	// Con r abre un archivo para lectura.
+	ptrArchivo = fopen(nombre_arch, "r");
+	if( ptrArchivo != NULL ){
+		existe = 1;
+		cerrar_archivo(ptrArchivo, nombre_arch);
+	}	
+	return existe;
+}
+
+int crear_archivo(char *nombre_arch){
+	int creado = 0;
+	FILE *ptrArchivo;
+	ptrArchivo = fopen(nombre_arch, "w");
+	if( ptrArchivo == NULL ){
+		// con w crea un archivo para escritura. Si el archivo ya existe, descarta el contenido actual.
+		printf("----> El archivo -> %s <- no pudo crearse (abrir; uso de w).\n", nombre_arch);
+	}	
+	else{
+		creado = 1;
+		printf("----> Archivo -> %s <- Creado (abierto; uso de w).\n", nombre_arch);
+		cerrar_archivo(ptrArchivo, nombre_arch);
+	}
+	return creado;
+}
+
+void liberar (){
+	struct nodo *reco = raiz;
+	struct nodo *bor;
+	while (reco != NULL){
+		bor = reco;
+		reco = reco->sig;
+		free(bor);
+	}	
+}
+
+int vacia(){
+	if (raiz == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+void imprimir(){
+	struct nodo *reco=raiz;
+	while (reco != NULL){
+		printf("%s\n",reco->cadena);
+		reco=reco->sig;
+	}
+	printf("\n");
+}
+
+void insertar(char *x){
+	struct nodo *nuevo;
+	nuevo=malloc(sizeof(struct nodo));
+	strcpy(nuevo->cadena,x);
+	nuevo->sig=NULL;
+	if(raiz==NULL){
+		raiz=nuevo;
+	}else{
+		if (strcmp(x,raiz->cadena)<0){
+			nuevo->sig=raiz;
+			raiz=nuevo;
+		}else{
+			struct nodo *reco=raiz;
+			struct nodo *atras=raiz;
+			while (strcmp(x,reco->cadena)>0 && reco->sig !=NULL){
+				atras=reco;
+				reco=reco->sig;
+			}
+			if(strcmp(x,reco->cadena)>0){
+				reco->sig=nuevo;
+			}else{
+				nuevo->sig=reco;
+				atras->sig=nuevo;
+			}
+		}
+	}
+}
+
+FILE *abrir_Archivo_solo_Lectura(char *nombre_arch){
+	FILE *ptrArchivo;
+	ptrArchivo = fopen(nombre_arch, "r");
+	if( ptrArchivo == NULL ){
+		// con r Abre un archivo para lectura.
+		printf("---> El archivo -> %s <- NO pudo abrirse (uso de r).\n", nombre_arch);
+	}	
+	else{
+		printf("---> Archivo -> %s <- Abierto (uso de r).\n", nombre_arch);
+	}
+	return ptrArchivo;
+}
+
+FILE *abrir_Archivo_lectura_escritura(char *nombre_arch){
+	FILE *ptrArchivo;
+	ptrArchivo = fopen(nombre_arch, "r+");
+	if( ptrArchivo == NULL ){
+		// con r+ Abre un archivo para actualización (lectura y escritura).
+		printf("---> El archivo -> %s <- NO pudo abrirse (uso de r+).\n", nombre_arch);
+	}	
+	else{
+		printf("---> Archivo -> %s <- Abierto (uso de r+).\n", nombre_arch);
+	}
+	return ptrArchivo;
+}
+
+void cerrar_archivo(FILE *ptrArchivo, char *nombre_arch){
+	fclose(ptrArchivo);
+	printf("----> Archivo -> %s <- Cerrado Correctamente.\n", nombre_arch);
+}
